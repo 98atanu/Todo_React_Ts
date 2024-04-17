5b295e7496cd5abf7f06e2adb5b8626691b0c606
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-04-17 12:37:12 +0530

    Project created.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AddTodo from "./components/AddTodo"
+
+const App = () => {
+  return (
+    <main>
+      <h1>Todo React + TypeScript</h1>
+      <AddTodo/>
+    </main>
+  )
+}
+
+export default App
